977eeb0de4ddbda140ad2c59925f1b76569112eb
Rancie1 <nathrancie@gmail.com>
2024-10-07 17:46:26 +1100

    validation checks create an array of errors

--- proccess_eoi.php
+++ proccess_eoi.php
@@ -21,144 +21,23 @@
     
     require_once "settings.php";
 
+    $errors=[];
+
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-            $err_msg = "";
 
-            if (isset($_POST['jobref']) && !empty($_POST['jobref'])) {
-                $jobReference = sanitize_input($_POST['jobref']);
-            }
-            if ($jobReference == ""){
-                $err_msg .= "<p>Job Reference is required.</p>";
-            } 
-            else if (!preg_match("/^[a-zA-Z0-9]{5}$/", $jobReference)) {
-                $err_msg .= "<p>Job Reference must be 5 characters long.</p>";
-            }
-
-
-
-            if (isset($_POST['fname']) && !empty($_POST['fname'])) {
-                $firstName= sanitize_input($_POST['fname']);
-            } 
-            if ($firstName == ""){
-                $err_msg .= "<p>First name is required.</p>";
-            } 
-            else if (!preg_match("/^[a-zA-Z]{1,20}$/", $firstName)) {
-                $err_msg .= "<p>First name must be 1-20 characters long.</p>";
-            }
-
-
-
-            if (isset($_POST['lname']) && !empty($_POST['lname'])) {
-                $lastName = sanitize_input($_POST['lname']);
-            }
-            if ($lastName == ""){
-                $err_msg .= "<p>Last name is required.</p>";
-            } 
-            else if (!preg_match("/^[a-zA-Z]{1,20}$/", $lastName)) {
-                $err_msg .= "<p>Last name must be 1-20 characters long.</p>";
-            }
-
-
-
-            if (isset($_POST['dob']) && !empty($_POST['dob'])) {
-                $dob = sanitize_input($_POST['dob']);
-            } 
-            if ($dob == ""){
-                $err_msg .= "<p>Date of birth is required.</p>";
-            } 
-            else if (!preg_match("/^(\d{4})-(\d{2})-(\d{2})$/", $dob)) {
-                $err_msg .= "<p>Date of birth must be in the format YYYY-MM-DD.</p>";
-            }
-
-
-
-            if (isset($_POST['Gender']) && !empty($_POST['Gender'])){
-                $gender = $_POST['Gender'];
-            }
-
-            if ($gender == ""){
-                $err_msg .= "<pGender is required.</p>";
-            } 
-
-
-
-
-            if (isset($_POST['street']) && !empty($_POST['street'])) {
-                $streetAddress = sanitize_input($_POST['street']);
-            } 
-            if ($streetAddress == ""){
-                $err_msg .= "<p>Street Address is required.</p>";
-            } 
-            else if (!preg_match("/^[a-zA-Z0-9\s]{1,40}$/", $streetAddress)) {
-                $err_msg .= "<p>Street Address must be 1-40 characters long.</p>";
-            }
-
-
-  
-            if (isset($_POST['suburb']) && !empty($_POST['suburb'])) {
-                $suburb = sanitize_input($_POST['suburb']);
-            } 
-            if ($suburb == ""){
-                $err_msg .= "<p>Suburb is required.</p>";
-            } 
-            else if (!preg_match("/^[a-zA-Z\s]{1,40}$/", $suburb)) {
-                $err_msg .= "<p>Suburb must be 1-40 characters long.</p>";
-            }
-
-
-            $states = ["VIC", "NSW", "QLD", "NT", "WA", "SA", "TAS", "ACT"];
-            
-            if (isset($_POST['state']) && !empty($_POST['state'])) {
-                $state = $_POST['state'];
-            }
-            if ($state == ""){
-                $err_msg .= "<p>State is required.</p>";
-            } 
-            else if (!in_array($state, $states)) {
-                $err_msg .= "<p>State must be one of VIC, NSW, QLD, NT, WA, SA, TAS, ACT.</p>";
-            }
-
-
-
-
-            if (isset($_POST['postcode']) && !empty($_POST['postcode'])) {
-                $postcode = sanitize_input($_POST['postcode']);
-            } 
-            if ($postcode == ""){
-                $err_msg .= "<p>Postcode is required.</p>";
-            } 
-            else if (!preg_match("/^\d{4}$/", $postcode)) {
-                $err_msg .= "<p>Postcode must be 4 digits long.</p>";
-            }
-
-
-
-
-            if (isset($_POST['email']) && !empty($_POST['email'])) {
-                $email = sanitize_input($_POST['email']);
-            } 
-            if ($email == ""){
-                $err_msg .= "<p>Email is required.</p>";
-            } 
-            else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $err_msg .= "<p>Invalid email format.</p>";
-            }
-
-
-
-            if (isset($_POST['phone']) && !empty($_POST['phone'])) {
-                $phone = sanitize_input($_POST['phone']);
-            } 
-            if ($phone == ""){
-                $err_msg .= "<p>Phone number is required.</p>";
-            } 
-            else if (!preg_match("/^[\d\s]{8,12}$/", $phone)) {
-                $err_msg .= "<p>Phone number must be 8 to 12 digits long.</p>";
-            }
-
-            
+            $jobReference = sanitize_input($_POST['jobref']);
+            $firstName= sanitize_input($_POST['fname']);
+            $lastName = sanitize_input($_POST['lname']);
+            $dob = $_POST['dob'];
+            $gender = $_POST['Gender'];
+            $streetAddress = sanitize_input($_POST['street']);
+            $suburb = sanitize_input($_POST['suburb']);
+            $state = $_POST['state'];
+            $postcode = sanitize_input($_POST['postcode']);
+            $email = sanitize_input($_POST['email']);
+            $phone = sanitize_input($_POST['phone']);
             $html = $_POST['html'];
             $css = $_POST['css'];
             $js = $_POST['js'];
@@ -167,16 +46,144 @@
             $otherSkill = $_POST['otherskills'];
             $otherSkillDesc = sanitize_input($_POST['skilldescription']);
 
+        
+
+           
+            if (empty($jobReference)){
+                $errors[] = "Job Reference is required.<br>";
+            } 
+            else if (!preg_match('/^\d{5}$/', $jobReference)) {
+                $errors[] = "Job Reference must be 5 digit number.<br>";
+            }
+
+
+
+            
+            if (empty($firstName)){
+                $errors[] = "First name is required.<br>";
+            } 
+            else if (!preg_match("/^[a-zA-Z]{1,20}$/", $firstName)) {
+                $errors[] = "First name must be 1-20 characters long.<br>";
+            }
+
+
+
+           
+            if (empty($lastName)){
+                $errors[] = "Last name is required.<br>";
+            } 
+            else if (!preg_match("/^[a-zA-Z]{1,20}$/", $lastName)) {
+                $errors[] = "Last name must be 1-20 characters long.<br>";
+            }
+
+
+
+         
+            if (empty($dob)){
+                $errors[] = "Date of birth is required.<br>";
+            } 
+            else if (!preg_match("/^(\d{4})-(\d{2})-(\d{2})$/", $dob)) {
+                $errors[] = "Date of birth must be in the format YYYY-MM-DD.<br>";
+            }
+
+
+
+           
+
+            if (empty($gender)){
+                $errors[] = "Gender is required.<br>";
+            } 
+
+
+
+
+            if (empty($streetAddress)){
+                $errors[] = "Street Address is required.<br>";
+            } 
+            else if (!preg_match("/^[a-zA-Z0-9\s]{1,40}$/", $streetAddress)) {
+                $errors[] = "Street Address must be 1-40 characters long.<br>";
+            }
+
+
+  
+          
+            if (empty($suburb)){
+                $errors[] = "Suburb is required.<br>";
+            } 
+            else if (!preg_match("/^[a-zA-Z\s]{1,40}$/", $suburb)) {
+                $errors[] = "Suburb must be 1-40 characters long.<br>";
+            }
+
+
+            $states = ["VIC", "NSW", "QLD", "NT", "WA", "SA", "TAS", "ACT"];
+            
+           
+            if (empty($state)){
+                $errors[] = "State is required.<br>";
+            } 
+            else if (!in_array($state, $states)) {
+                $errors[] = "State must be one of VIC, NSW, QLD, NT, WA, SA, TAS, ACT.<br>";
+            }
+
+
+
+
+            
+            if (empty($postcode)){
+                $errors[] = "Postcode is required.<br>";
+            } 
+            else if (!preg_match("/^\d{4}$/", $postcode)) {
+                $errors[] = "Postcode must be 4 digits long.<br>";
+            }
+
+
+
+
+          
+            if (empty($email)){
+                $errors[] = "Email is required.<br>";
+            } 
+            else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $errors[] = "Invalid email.<br>";
+            }
+
+
+
+           
+            if (empty($phone)){
+                $errors[] = "Phone number is required.<br>";
+            } 
+            else if (!preg_match("/^[\d\s]{8,12}$/", $phone)) {
+                $errors[] = "Phone number must be 8 to 12 digits long.<br>";
+            }
+
+           
+     
+
             if (isset($otherSkill) && empty($otherSkillDesc)){
-                $err_msg .= "<p>Other Skills Description is required when selecting Other Skill.</p>";
+                $errors[] = "Other Skills Description is required when selecting Other Skill.<br>";
                 
             }
 
-            if (!empty($err_msg)){
-                echo $err_msg;
-                echo "<p>Go back to <a href='apply.html'>Application Form</a></p>";
-        
+
+
+            if(!empty($errors)){
+                echo "<h1>Errors</h1>";
+
+                echo "<ul>";
+                foreach ($errors as $error){
+                    echo "<li>$error</li>";
+                }
+                echo "</ul>";
+
+
             }
+
+          
+
+
+
+
 
             if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 echo '<pre>';
